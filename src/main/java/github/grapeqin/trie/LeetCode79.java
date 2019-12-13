10eb1eace50b620814664288354fc58ca7c9a927
grapeqin <qinzhengyan312@gmail.com>
2019-12-13 10:18:01 +0800

    单词搜索代码优化

--- src/main/java/github/grapeqin/trie/LeetCode79.java
+++ src/main/java/github/grapeqin/trie/LeetCode79.java
@@ -9,13 +9,14 @@ package github.grapeqin.trie;
  */
 public class LeetCode79 {
 
-  private int[][] direction = new int[][] {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+  private static final char PLACEHOLDER = '+';
+
+  private int[][] direction = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
   public boolean exist(char[][] board, String word) {
-    boolean[][] exist = new boolean[board.length][board[0].length];
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[0].length; j++) {
-        if (dfs(board, exist, i, j, word, 0)) {
+        if (dfs(board, word, i, j, 0)) {
           return true;
         }
       }
@@ -26,35 +27,39 @@ public class LeetCode79 {
   /**
    * 深度优先搜索
    *
-   * @param board 原网格
-   * @param exist 判断网格单元是否被占用
    * @param row 横坐标
    * @param col 纵坐标
-   * @param word 目标单词
-   * @param start 判断起点
+   * @param index 字符串的每个字符下标
    * @return
    */
-  private boolean dfs(char[][] board, boolean[][] exist, int row, int col, String word, int start) {
-    if (start == word.length() - 1) {
-      return board[row][col] == word.charAt(start);
+  private boolean dfs(char[][] board, String word, int row, int col, int index) {
+    // 判断到最后一个字符
+    if (index == word.length() - 1) {
+      // 返回的结果取决于最后一个字符是否相等
+      return board[row][col] == word.charAt(index);
     }
-    if (word.charAt(start) == board[row][col]) {
-      exist[row][col] = true;
-      for (int k = 0; k < 4; k++) {
+    if (word.charAt(index) == board[row][col]) {
+      // 保护现场
+      char c = board[row][col];
+      // 记录已经走过的网格
+      board[row][col] = PLACEHOLDER;
+      for (int k = 0; k < direction.length; k++) {
+        // 当前单元格上下左右进行询问
         int x = row + direction[k][0];
         int y = col + direction[k][1];
-        if (validate(x, y, board, exist)) {
-          if (dfs(board, exist, x, y, word, start + 1)) {
+        if (validate(board, x, y)) {
+          if (dfs(board, word, x, y, index + 1)) {
             return true;
           }
         }
       }
-      exist[row][col] = false;
+      // 恢复现场
+      board[row][col] = c;
     }
     return false;
   }
 
-  private boolean validate(int row, int col, char[][] board, boolean[][] exist) {
+  private boolean validate(char[][] board, int row, int col) {
     // 横坐标越界
     if (row < 0 || row >= board.length) {
       return false;
@@ -64,7 +69,7 @@ public class LeetCode79 {
       return false;
     }
     // 当前单元格已经使用了
-    if (exist[row][col]) {
+    if (board[row][col] == PLACEHOLDER) {
       return false;
     }
     return true;
@@ -80,8 +85,20 @@ public class LeetCode79 {
         };
     String word = "ABCCED";
     System.out.println(leetCode79.exist(board, word));
+    board =
+        new char[][] {
+          {'A', 'B', 'C', 'E'},
+          {'S', 'F', 'C', 'S'},
+          {'A', 'D', 'E', 'E'}
+        };
     word = "SEE";
     System.out.println(leetCode79.exist(board, word));
+    board =
+        new char[][] {
+          {'A', 'B', 'C', 'E'},
+          {'S', 'F', 'C', 'S'},
+          {'A', 'D', 'E', 'E'}
+        };
     word = "ABCB";
     System.out.println(leetCode79.exist(board, word));
 
