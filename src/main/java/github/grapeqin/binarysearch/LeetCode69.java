9ed4a3d1ef3ad1601b44c1103b17ce4006756061
grapeqin <qinzhengyan312@gmail.com>
2019-12-13 22:59:06 +0800

    求x的平方根代码优化

--- src/main/java/github/grapeqin/binarysearch/LeetCode69.java
+++ src/main/java/github/grapeqin/binarysearch/LeetCode69.java
@@ -10,9 +10,8 @@ package github.grapeqin.binarysearch;
 public class LeetCode69 {
 
   /**
-   * 求x的平方根，忽略小数位
-   * 思路：参考二分查找的代码模板：定义left=0,right=x，
-   * 当left<right时依次迭代尝试，直到查找到目标值
+   * 求x的平方根，忽略小数位 思路：参考二分查找的代码模板：定义left=0,right=x， 当left<right时依次迭代尝试，直到查找到目标值
+   *
    * @param x 任意非负整数
    * @return
    */
@@ -30,21 +29,19 @@ public class LeetCode69 {
     return left;
   }
 
-	/**
-	 * 求x的平方根，使用牛顿法
-	 * 看推导之前，请先了解导数的基本定义和牛肚法的基本概念之后再来推导计算x的公式
-	 * 根据导数的公式斜率F(x0) = f(x0) / xi - x0  若 F(xi) 约等于0，要找到这个xi 则
-	 * xi = f(x0) / F(x0) + x0 同时 f(x) = x的平方 - a(初始值x0) 且 平方根的斜率为2
-	 * 所以 xi = (x0+a/x0) / 2
-	 * @param x
-	 * @return
-	 */
-  public int mySqrt(long x){
-	long a = x;
-	while (x * x > a){
-		x = (x + a / x ) / 2;
-	}
-	return (int)x;
+  /**
+   * 求x的平方根，使用牛顿法 看推导之前，请先了解导数的基本定义和牛顿法的基本概念之后再来推导计算x的公式 根据导数的公式斜率F(x0) = f(x0) / xi - x0 若 F(xi)
+   * 约等于0，要找到这个xi 则 xi = f(x0) / F(x0) + x0 同时 f(x) = x的平方 - a(初始值x0) 且 平方根的斜率为2 所以 xi = (x0+a/x0) /
+   * 2
+   *
+   * @param x
+   * @return
+   */
+  public int mySqrt(long x) {
+    long a = x;
+    while (x * x > a) {
+      x = (x + a / x) / 2;
+    }
+    return (int) x;
   }
-
 }
