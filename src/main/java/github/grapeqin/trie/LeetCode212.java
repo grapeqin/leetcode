18fa2741e189ef0cd913f6129f8fe657fa7e60a9
grapeqin <qinzhengyan312@gmail.com>
2019-12-13 10:43:24 +0800

    单词搜索II代码优化

--- src/main/java/github/grapeqin/trie/LeetCode212.java
+++ src/main/java/github/grapeqin/trie/LeetCode212.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 单词搜索II 需要引用 LeetCode209中定义的Trie结构
+ * 单词搜索II
+ *
+ * <p>思路1 可以借助LeetCode79中的函数 针对列表中每个单词进行一次递归，这样子每个单词在棋盘上要递归一遍，效率比较差
+ *
+ * <p>思路2 将要匹配的单词列表构造成一棵Trie树，利用Trie树的search和startWith方法可以实现只遍历一次棋盘即可得到结果
+ *
+ * <p>需要引用 LeetCode209中定义的Trie结构
  *
  * @description
  * @author qinzy
@@ -12,7 +18,9 @@ import java.util.List;
  */
 public class LeetCode212 {
 
-  private int[][] direction = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+  private static final char PLACEHOLDER = '+';
+
+  private int[][] direction = new int[][] {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
   private Trie trie;
 
@@ -45,7 +53,6 @@ public class LeetCode212 {
   private void dfs(List<String> ans, String cur, int row, int col) {
     char c = board[row][col];
     cur += c;
-
     if (trie.search(cur) && !ans.contains(cur)) {
       ans.add(cur);
     }
@@ -53,16 +60,35 @@ public class LeetCode212 {
     if (!trie.startsWith(cur)) {
       return;
     }
-
-    board[row][col] = '+';
-    for (int k = 0; k < 4; k++) {
+    board[row][col] = PLACEHOLDER;
+    for (int k = 0; k < direction.length; k++) {
       int x = row + direction[k][0];
       int y = col + direction[k][1];
-      if (x >= 0 && x < board.length && y >= 0 && y < board[0].length && board[x][y] != '+') {
+      if (valid(x, y)) {
         dfs(ans, cur, x, y);
       }
     }
     board[row][col] = c;
+  }
+
+  /**
+   * 验证坐标位置的合法性
+   *
+   * @param row
+   * @param col
+   * @return
+   */
+  private boolean valid(int row, int col) {
+    if (row < 0 || row >= board.length) {
+      return false;
+    }
+    if (col < 0 || col >= board[0].length) {
+      return false;
+    }
+    if (board[row][col] == PLACEHOLDER) {
+      return false;
+    }
+    return true;
   }
 
   public static void main(String[] args) {
