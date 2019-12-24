72a2531f7de195c025357c6234f68a15ca6dc0b7
grapeqin <qinzhengyan312@gmail.com>
2019-12-24 17:46:55 +0800

    Fibonacci数列问题

--- src/main/java/github/grapeqin/dynamicprogramming/FibonaccoTest.java
+++ src/main/java/github/grapeqin/dynamicprogramming/FibonaccoTest.java
@@ -1,6 +1,8 @@
 package github.grapeqin.dynamicprogramming;
 
 /**
+ * Fibonacci的几种解法
+ *
  * @description
  * @author qinzy
  * @date 2019-12-16
@@ -8,7 +10,7 @@ package github.grapeqin.dynamicprogramming;
 public class FibonaccoTest {
 
   /**
-   * 递归模式
+   * 1.递归模式 时间复杂度为2的n次方
    *
    * @param n
    * @return
@@ -86,6 +88,64 @@ public class FibonaccoTest {
     return c;
   }
 
+  /**
+   * 求矩阵的n-1次幂
+   *
+   * <p>时间复杂度为logN
+   *
+   * @param n
+   * @return
+   */
+  public int fibonacci5(int n) {
+    if (n < 2) {
+      return n;
+    }
+    int[][] matrix = new int[][] {{1, 1}, {1, 0}};
+    recurse(matrix, n - 1);
+    return matrix[0][0];
+  }
+
+  /**
+   * 递归求X的N次幂
+   *
+   * <p>参考链接：
+   * https://leetcode-cn.com/problems/fibonacci-number/solution/fei-bo-na-qi-shu-by-leetcode/
+   *
+   * @param matrix
+   * @param n
+   */
+  private void recurse(int[][] matrix, int n) {
+    if (n < 2) {
+      return;
+    }
+    recurse(matrix, n / 2);
+    multiply(matrix, matrix);
+
+    int[][] a = new int[][] {{1, 1}, {1, 0}};
+    // 基数
+    if ((n & 1) == 1) {
+      multiply(matrix, a);
+    }
+  }
+
+  /**
+   * 矩阵求和
+   *
+   * @param a
+   * @param b
+   */
+  private void multiply(int[][] a, int[][] b) {
+    int x = a[0][0] * b[0][0] + a[0][1] * b[1][0];
+    int y = a[0][0] * b[0][1] + a[0][1] * b[1][1];
+    int z = a[1][0] * b[0][0] + a[1][1] * b[1][0];
+    int w = a[1][0] * b[0][1] + a[1][1] * b[1][1];
+
+    a[0][0] = x;
+    a[0][1] = y;
+    a[1][0] = z;
+    a[1][1] = w;
+  }
+
   public static void main(String[] args) {
     FibonaccoTest test = new FibonaccoTest();
     int n = 40;
@@ -98,5 +158,6 @@ public class FibonaccoTest {
     System.out.println(test.fibonacci4(n) == test.fibonacci(n));
     System.out.println(test.fibonacci4(n) == test.fibonacci2(n));
     System.out.println(test.fibonacci4(n) == test.fibonacci3(n));
+    System.out.println(test.fibonacci5(n) == test.fibonacci3(n));
   }
 }
