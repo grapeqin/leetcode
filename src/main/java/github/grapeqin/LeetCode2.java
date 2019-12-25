1dad9bff391a49409c782f47bfc3c76ffd3477aa
grapeqin <qinzhengyan312@gmail.com>
2019-12-25 17:15:41 +0800

    两数相加

---
+++ src/main/java/github/grapeqin/LeetCode2.java
@@ -0,0 +1,42 @@
+package github.grapeqin;
+
+/**
+ * 两数相加
+ *
+ * @description
+ * @author qinzy
+ * @date 2019-12-24
+ */
+public class LeetCode2 {
+
+  private static class ListNode {
+    private int val;
+    private ListNode next;
+
+    ListNode(int x) {
+      val = x;
+    }
+  }
+
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode p1 = l1;
+    ListNode p2 = l2;
+    ListNode head = new ListNode(0);
+    ListNode cur = head;
+    int sum = 0;
+    while (null != p1 || null != p2 || sum > 0) {
+      if (null != p1) {
+        sum += p1.val;
+        p1 = p1.next;
+      }
+      if (null != p2) {
+        sum += p2.val;
+        p2 = p2.next;
+      }
+      cur.next = new ListNode(sum % 10);
+      cur = cur.next;
+      sum /= 10;
+    }
+    return head.next;
+  }
+}
